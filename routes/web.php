f87058145273842e954a88c37aa309d9a09b0d39
Shnayzi <bmtinana@gmail.com>
2025-02-25 19:59:38 -0300

    projeto laravel

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
+
+Route::get('/login', [\App\Http\Controllers\LoginController::class, 'login']);
+
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+
+Route::get('/contato', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+
+Route::get('/clientes', [\App\Http\Controllers\ClientesController::class, 'clientes']);
+
+Route::get('/fornecedores', [\App\Http\Controllers\FornecedoresController::class, 'fornecedores']);
+
+Route::get('/produtos', function () {
+    return view('welcome');
+});
+
